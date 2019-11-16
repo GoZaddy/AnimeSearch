8d3657ffeaf0e4696c5b33261ca0636ff0ab13b7
faruq2 <faruqyusuff437@gmail.com>
2019-11-16 13:03:41 +0100

    Updates

---
+++ precache-manifest.3b29a7858a71568057bc42b9a85d1bcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e611f4122726c3268d466cb72b5c167a",
+    "url": "/AnimeSearch/index.html"
+  },
+  {
+    "revision": "48ede5989857a7025bb5",
+    "url": "/AnimeSearch/static/css/main.61a590f7.chunk.css"
+  },
+  {
+    "revision": "84fc04fb0203c550a3f6",
+    "url": "/AnimeSearch/static/js/2.16d886f3.chunk.js"
+  },
+  {
+    "revision": "48ede5989857a7025bb5",
+    "url": "/AnimeSearch/static/js/main.a84891de.chunk.js"
+  },
+  {
+    "revision": "b6635f24127ba1d122a6",
+    "url": "/AnimeSearch/static/js/runtime-main.c697d007.js"
+  }
+]);
